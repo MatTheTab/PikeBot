f3bca49a420b11b7fa14714a0ad9cdf30b0d2b56
bpukacki <bpukacki@man.poznan.pl>
2024-03-11 23:10:08 +0100

    Add test templates

---
+++ tests/test_chess_playing.py
@@ -0,0 +1,20 @@
+import pytest
+from utils.chess_utils import *
+
+class TestChessPlaying:
+    stockfish_path = r"C:\Users\barte\Desktop\Studies\chess_bot\engine\stockfish\stockfish-windows-x86-64-avx2.exe"
+    
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        self.board = chess.Board()
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.stockfish_path)
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        self.engine.quit()
+        del self.engine
+
+    def test_one(self):
+        move_scores = evaluate_all_moves_simple_engine(self.board, self.engine)
+        print("Move Scores:", move_scores)
+        assert len(move_scores) > 0
